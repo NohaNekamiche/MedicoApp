54becc3ca45d7e7681b363af30f5676f105d65ac
Selmane Lamia <hl_selmane@esi.dz>
2021-06-15 17:15:10 +0100

    adding slapsh done

--- app/src/main/java/com/example/medico/SplashActivity.kt
+++ app/src/main/java/com/example/medico/SplashActivity.kt
@@ -1,12 +1,20 @@
-package com.example.medico.ui
+package com.example.medico
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.medico.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+             val intent = Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(intent)
+        },3000)
     }
 }
