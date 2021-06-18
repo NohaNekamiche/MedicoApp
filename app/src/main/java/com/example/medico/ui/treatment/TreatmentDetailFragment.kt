33dfba5321ee83b007e3ca9df5d6e21a8ea9733a
Selmane Lamia <hl_selmane@esi.dz>
2021-06-18 14:21:31 +0100

    added phone and map code not tested yet

--- app/src/main/java/com/example/medico/ui/treatment/TreatmentDetailFragment.kt
+++ app/src/main/java/com/example/medico/ui/treatment/TreatmentDetailFragment.kt
@@ -20,22 +20,34 @@ class TreatmentDetailFragment : Fragment() {
 
         }
     }
-    val uri = Uri.parse("tel:" +doc_phone)
-    val intent = Intent(Intent.ACTION_CALL, uri)
 
-    /*  if (intent.resolveActivity(context.packageManager) != null) {
-        requireActivity().startActivity(intent)
-    }*/
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        doc_phone.setOnClickListener {
 
+        doc_phone.setOnClickListener {
+            val uri = Uri.parse("tel:" +doc_phone)
+            val intent = Intent(Intent.ACTION_CALL, uri)
+
+            /*  if (intent.resolveActivity(context.packageManager) != null) {
+                requireActivity().startActivity(intent)
+            }*/
         }
+
+           /* val lat=data[position].lat
+            val lang=data[position].lang
+         doc_pos.setOnClickListener{ view->
+                val url=Uri.parse("geo:$lat,$lang")
+                val intent= Intent(Intent.ACTION_VIEW,url)
+                context.startActivity(intent)
+            }*/
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_treatment_detail, container, false)
     }
+
+
 
 
 }
