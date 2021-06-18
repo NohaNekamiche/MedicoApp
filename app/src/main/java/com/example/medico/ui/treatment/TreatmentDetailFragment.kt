426f413947e59fba993b6f8b575e8a8e6b4dc556
Selmane Lamia <hl_selmane@esi.dz>
2021-06-18 14:18:30 +0100

    permission added

--- app/src/main/java/com/example/medico/ui/treatment/TreatmentDetailFragment.kt
+++ app/src/main/java/com/example/medico/ui/treatment/TreatmentDetailFragment.kt
@@ -1,11 +1,14 @@
 package com.example.medico.ui.treatment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.medico.R
+import kotlinx.android.synthetic.main.fragment_treatment_detail.*
 
 
 class TreatmentDetailFragment : Fragment() {
@@ -17,11 +20,19 @@ class TreatmentDetailFragment : Fragment() {
 
         }
     }
+    val uri = Uri.parse("tel:" +doc_phone)
+    val intent = Intent(Intent.ACTION_CALL, uri)
 
+    /*  if (intent.resolveActivity(context.packageManager) != null) {
+        requireActivity().startActivity(intent)
+    }*/
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        doc_phone.setOnClickListener {
+
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_treatment_detail, container, false)
     }
